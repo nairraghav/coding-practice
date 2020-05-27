26c4f1f4f2766770855d82da4fd7b544a16b8343
Raghav Nair <nairraghav@hotmail.com>
2020-05-26 17:55:55 -0700

    Adding two new easy problems and moving others

---
+++ hackerrank/python/easy/capitalize.py
@@ -0,0 +1,60 @@
+"""
+https://www.hackerrank.com/challenges/capitalize/problem
+
+You are asked to ensure that the first and last names of people begin with a capital letter in their passports. For example, alison heck should be capitalised correctly as Alison Heck.
+
+
+Given a full name, your task is to capitalize the name appropriately.
+
+Input Format
+
+A single line of input containing the full name, .
+
+Constraints
+
+The string consists of alphanumeric characters and spaces.
+Note: in a word only the first character is capitalized. Example 12abc when capitalized remains 12abc.
+
+Output Format
+
+Print the capitalized string, .
+
+Sample Input
+
+chris alan
+Sample Output
+
+Chris Alan
+"""
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    previous_space = True
+    for char_index in range(len(s) - 1):
+        if previous_space:
+            s = s[:char_index] + s[char_index].upper() + s[char_index+1:]
+        previous_space = (s[char_index] == " ")
+    if previous_space:
+        s = s[:-1] + s[-1].upper()
+    return s
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
